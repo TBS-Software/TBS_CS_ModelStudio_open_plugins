c9127c1eb70d944513e913615b7244e0028ed104
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2023-04-04 20:56:52 +0300

    uploading test console app
    Приложение сугубо для проверки логики кода перед реализацией. Можно удалить или выгрузить

---
+++ src/test_app/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static MS_Tools.MS_Viper.ViperTools;
+
+namespace test_app
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			test();
+			Console.WriteLine("End!");
+			Console.ReadKey();
+		}
+		static void test()
+		{
+			//WrAxisElementsActions(Work_type.ChangeInsulationByValue, 27);
+
+			//WrAxisElementsActions(Work_type.DisableShowingInsulation);
+			//WrAxisElementsActions(Work_type.EnableShowingInsulation);
+			//WrAxisElementsActions(Work_type.ChangeInsulationByPart);
+			//WrAxisElementsActions(Work_type.ChangeInsulationByAxis);
+			//WrAxisElementsActions(Work_type.ChangeInsulationOfPartsByAxis);
+
+		}
+	}
+}
